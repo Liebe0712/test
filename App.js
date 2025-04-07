2e77792b0f34cad1b4c6d083074924ac4656c264
Liebe0712 <abayasaa0712@gmail.com>
2025-04-07 17:26:30 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Login from "./components/Food"; 
+import "./Login.css"; 
+function App() {
+  return (
+    <div className="App">
+      <Login /> {}
+    </div>
+  );
+}
+
+export default App;
